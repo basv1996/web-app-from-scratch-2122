2cf3d3d40eba7e6bdd96df8e3926cdf3689a1642
basv1996 <bas.vugts@gmail.com>
2022-02-07 20:39:08 +0100

    the flip works

---
+++ visitekaartje/scripts/script.js
@@ -0,0 +1,24 @@
+const checkbox = document.querySelector('.checkbox');
+const section = document.querySelector('section')
+const frontCard = document.querySelector('.front')
+const backCard = document.querySelector('.back')
+
+console.log(checkbox)
+console.log(section)
+
+function checkChecked(){
+    if(checkbox.checked == true){
+        //frontCard.style.transform = "rotateY(180deg)"
+        // backCard.style.transform = "rotateY(180deg)"
+        section.style.transform = "rotateY(180deg)"
+        //frontCard.style.opacity = "0"
+        console.log("check")
+    } else {
+        section.style.transform = "rotateY(0deg)"
+    console.log("nothing")
+    }
+}
+
+// checkChecked();
+
+checkbox.addEventListener("click", checkChecked)
