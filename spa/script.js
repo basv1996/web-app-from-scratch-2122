d8dd25711a07f544c1db0b52cc08fbf02c9e83cc
basv1996 <bas.vugts@gmail.com>
2022-02-14 13:28:54 +0100

    capture image

--- spa/script.js
+++ spa/script.js
@@ -1,3 +1,7 @@
+barcode = "8715600243949"
+API_url = `https://world.openfoodfacts.org/api/v0/product/${barcode}.json`
+
+
 // check compatibility
 if (!('BarcodeDetector' in window)) {
     console.log('Barcode Detector is not supported by this browser.');
@@ -11,4 +15,13 @@ if (!('BarcodeDetector' in window)) {
     // create new detector
     var barcodeDetector = new BarcodeDetector({formats: ['code_39', 'codabar', 'ean_13']});
   }
+
+// get barcode
+fetch(API_url)
+  .then(results => results.json())
+  .then(data => {
+      console.log(data)
+    })
+  .catch(error => console.log(error))
+
   
