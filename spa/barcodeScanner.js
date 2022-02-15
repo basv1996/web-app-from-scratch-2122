788fe2921d6b7e5b5d5abedb107a4f50547af5da
basv1996 <bas.vugts@gmail.com>
2022-02-15 11:10:48 +0100

    new nutrient test

--- spa/barcodeScanner.js
+++ spa/barcodeScanner.js
@@ -41,7 +41,8 @@ window.onload = () => {
                       nutriscore_fat: response.product.nutrient_levels.fat,
                       nutriscore_salt: response.product.nutrient_levels.salt,
                       nutriscore_sugars: response.product.nutrient_levels.sugars,
-                      nutrient_kcal100gram: response.product.nutriments[energy-kcal_100gr],
+                      //nutrient_kcal100gram: response.product.nutriments[energy-kcal_100gr],
+                      // <p>kcal per 100gr: : ${product.nutrient_kcal100gram}</p>
                       img: response.product.image_front_url
                   }
           
@@ -55,7 +56,7 @@ window.onload = () => {
               <p>fat: ${product.nutriscore_fat}</p>
               <p>salt: ${product.nutriscore_salt}</p>
               <p>sugars: ${product.nutriscore}</p>
-              <p>kcal per 100gr: : ${product.nutrient_kcal100gram}</p>
+             
            </div>
           `;
           
