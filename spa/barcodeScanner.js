1464cd3c30d97b9e5aaa559fd003c516daa10a7e
basv1996 <bas.vugts@gmail.com>
2022-02-15 11:50:19 +0100

    more nutrients

--- spa/barcodeScanner.js
+++ spa/barcodeScanner.js
@@ -43,9 +43,11 @@ window.onload = () => {
                       // nutriscore_sugars: response.product.nutrient_levels.sugars,
                       kcal100gram: response.product.nutriments['energy-kcal_100g'],
                       carbsPer100gram: response.product.nutriments['carbohydrates_100g'],
+                      sugarspercarbs: response.product.nutriments['sugars_100g'],
                       fatPer100gram: response.product.nutriments['fat_100g'],
                       proteinsPer100gram: response.product.nutriments['proteins_100g'],
                       sugarsPer100gram: response.product.nutriments['sugars_100g'],
+
                      
                       nutriments: response.product.nutriments,
                       img: response.product.image_front_url
@@ -62,7 +64,8 @@ window.onload = () => {
               </h3>
               <h3> Nutriments: </h3>
               <p>kcal per 100gr:  ${product.kcal100gram}</p>
-              <p>Carbs per 100gr:  ${product.carbsPer100gram}</p>
+              <p>Total Carbohybrates per 100gr:  ${product.carbsPer100gram}</p>
+              <p class="toTheRight">sugars: ${product.sugarspercarbs}</p>
               <p>Fat per 100gr:  ${product.fatPer100gram}</p>
               <p>Sugar per 100gr:  ${product.sugarsPer100gram}</p>
            
