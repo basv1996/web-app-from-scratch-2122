3a955a008977e121a94b893e87f1c5db2e0c3876
basv1996 <bas.vugts@gmail.com>
2022-02-15 13:28:47 +0100

    try2

--- spa/barcodeScanner.js
+++ spa/barcodeScanner.js
@@ -65,9 +65,10 @@ window.onload = () => {
               <p>kcal per 100gr:  ${product.kcal100gram}</p>
               <p>Total Carbohybrates per 100gr:  ${product.carbsPer100gram}</p>
               <p class="toTheRight">sugars: ${product.sugarspercarbs}</p>
-              <p>Fat per 100gr:  ${product.fatPer100gram}</p>       
+              <p>Fat per 100gr:  ${product.fatPer100gram}</p> 
+              <p class="scanOtherCode"> Scan another code</p>      
            </div>
-           <p class="scanOtherCode">Scan another code</p>
+          
           `;
           
           document.querySelector("main section:nth-of-type(2)").innerHTML = markup;    
