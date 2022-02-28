79b9e463b5513a063ce49f4f1e0c2edeafaa79f4
basv1996 <bas.vugts@gmail.com>
2022-02-28 13:12:20 +0100

    module

--- spa/barcodeScanner.js
+++ spa/barcodeScanner.js
@@ -1,3 +1,5 @@
+import { getMyData } from "./scripts/getData"
+
   const barcodeBlock = document.querySelector(".scanCode")
   const fillInyourselfChose = document.querySelector(".fillInYourself")
   const choseYourMethod = document.querySelector(".choseYourMethod")
@@ -56,48 +58,8 @@
               // </svg>
               // </section>
               // `
-              
-              fetch(getURL)
-              .then(response => response.json())
-              //   {
-              //   return response.status >= 200 && response.status <= 299 ? response.json() : console.log('error')
-              // })
-               .then(response => {
-                  console.log(response.product)
-
-                  //skeletonSection.innerHTML = ''
-          
-                  const product = {
-                      name: response.product.product_name,
-                      brand: response.product.brand_owner,
-                      kcal100gram: response.product.nutriments['energy-kcal_100g'],
-                      carbsPer100gram: response.product.nutriments['carbohydrates_100g'],
-                      sugarspercarbs: response.product.nutriments['sugars_100g'],
-                      fatPer100gram: response.product.nutriments['fat_100g'],
-                      proteinsPer100gram: response.product.nutriments['proteins_100g'],
-                                          
-                      nutriments: response.product.nutriments,
-                      img: response.product.image_front_url
-                  }
-          
-                  const markup = `
-                    <div class="product">
-                      <img src=${product.img}>
-                        <h2>
-                           <b>Name:</b> ${product.name} 
-                         </h2>
-                          <h3> Nutriments: </h3>
-                            <p>kcal per 100gr:  ${product.kcal100gram}</p>
-                            <p>Total Carbohybrates per 100gr:  ${product.carbsPer100gram}</p>
-                            <p class="toTheRight">sugars: ${product.sugarspercarbs}</p>
-                            <p>Fat per 100gr:  ${product.fatPer100gram}</p> 
-                            <p class="backToOriginal"><a href=".">Scan another code</a></p>      
-                      </div>
-                  `;
-          
-          document.querySelector("main section:nth-of-type(2)").innerHTML = markup;    
-              })
-              .catch(error => document.body.insertAdjacentHTML('beforebegin', error))
+              getMyData(video, getURL, loadingElement)
+           
             }
           });
         })
