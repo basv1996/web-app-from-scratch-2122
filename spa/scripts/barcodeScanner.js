78d63d71f3d908b2f04d2a8deeba78928a9acdd5
basv1996 <bas.vugts@gmail.com>
2022-03-07 11:35:24 +0100

    without skeleotn

--- spa/scripts/barcodeScanner.js
+++ spa/scripts/barcodeScanner.js
@@ -38,19 +38,19 @@ import { getMyData } from "./modules/getData.js"
               const newBarcode = barcode.rawValue; 
               const getURL = 'https://world.openfoodfacts.org/api/v0/product/' + newBarcode + '.json'
               
-              skeletonSection.innerHTML = `
-              <section>
-              <svg width='100%' height='90vh' class="skeletor">
-                           <rect width='100%' height='30vh' />
-                           <rect transform='translate(20, 375)' width='30%' height='2em' />
-                           <rect transform='translate(20, 440)' width='30%' height='1.5em' />
-                           <rect transform='translate(20, 470)' width='25%' height='1em' />
-                           <rect transform='translate(20, 490)' width='25%' height='1em' />
-                           <rect transform='translate(20, 510)' width='25%' height='1em' />
-                           <rect transform='translate(20, 530)' width='25%' height='1em' />
-              </svg>
-              </section>
-              `
+              // skeletonSection.innerHTML = `
+              // <section>
+              // <svg width='100%' height='90vh' class="skeletor">
+              //              <rect width='100%' height='30vh' />
+              //              <rect transform='translate(20, 375)' width='30%' height='2em' />
+              //              <rect transform='translate(20, 440)' width='30%' height='1.5em' />
+              //              <rect transform='translate(20, 470)' width='25%' height='1em' />
+              //              <rect transform='translate(20, 490)' width='25%' height='1em' />
+              //              <rect transform='translate(20, 510)' width='25%' height='1em' />
+              //              <rect transform='translate(20, 530)' width='25%' height='1em' />
+              // </svg>
+              // </section>
+              // `
               getMyData(video, getURL, loadingElement)
            
             }
