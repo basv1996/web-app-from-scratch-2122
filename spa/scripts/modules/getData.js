0feafc29f746ea5f5e260238b7834811c1016124
basv1996 <bas.vugts@gmail.com>
2022-03-08 12:59:15 +0100

    product module

--- spa/scripts/modules/getData.js
+++ spa/scripts/modules/getData.js
@@ -1,4 +1,5 @@
-import { errorMsg } from "./error"
+import { errorMsg } from "./error.js"
+import { product } from "./product.js"
 export const getMyData = (video, getURL, loadingElement) =>{
 
 fetch(getURL)
@@ -11,20 +12,21 @@ fetch(getURL)
 
                      //skeletonSection.innerHTML = ''
           
-                     const product = {
-                      name: response.product.product_name,
-                      brand: response.product.brand_owner,
-                      kcal100gram: response.product.nutriments['energy-kcal_100g'],
-                      carbsPer100gram: response.product.nutriments['carbohydrates_100g'],
-                      sugarspercarbs: response.product.nutriments['sugars_100g'],
-                      fatPer100gram: response.product.nutriments['fat_100g'],
-                      proteinsPer100gram: response.product.nutriments['proteins_100g'],
-                      nutriScore: response.product.nutriscore_grade,
-                      novaGroup: response.product.nova_group,
+                  //    const product = {
+                  //     name: response.product.product_name,
+                  //     brand: response.product.brand_owner,
+                  //     kcal100gram: response.product.nutriments['energy-kcal_100g'],
+                  //     carbsPer100gram: response.product.nutriments['carbohydrates_100g'],
+                  //     sugarspercarbs: response.product.nutriments['sugars_100g'],
+                  //     fatPer100gram: response.product.nutriments['fat_100g'],
+                  //     proteinsPer100gram: response.product.nutriments['proteins_100g'],
+                  //     nutriScore: response.product.nutriscore_grade,
+                  //     novaGroup: response.product.nova_group,
                                           
-                      nutriments: response.product.nutriments,
-                      img: response.product.image_front_url
-                  }
+                  //     nutriments: response.product.nutriments,
+                  //     img: response.product.image_front_url
+                  // }
+                  product
           
                   const markup = `
                     <div class="product">
