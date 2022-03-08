96964748bf4aa8448d2643c48c740f6c02a2a693
basv1996 <bas.vugts@gmail.com>
2022-03-08 13:40:40 +0100

    skeleton back to original

--- spa/scripts/barcodeScanner.js
+++ spa/scripts/barcodeScanner.js
@@ -40,7 +40,20 @@ import { loader } from "./modules/loading.js"
               const newBarcode = barcode.rawValue; 
               const getURL = 'https://world.openfoodfacts.org/api/v0/product/' + newBarcode + '.json';
               
-              skeletonSection.insertAdjacentHTML("afterbegin", `
+              // skeletonSection.insertAdjacentHTML("afterbegin", `
+              // <section>
+              // <svg width='100%' height='90vh' class="skeletor">
+              //              <rect width='100%' height='30vh' />
+              //              <rect transform='translate(20, 375)' width='30%' height='2em' />
+              //              <rect transform='translate(20, 440)' width='30%' height='1.5em' />
+              //              <rect transform='translate(20, 470)' width='25%' height='1em' />
+              //              <rect transform='translate(20, 490)' width='25%' height='1em' />
+              //              <rect transform='translate(20, 510)' width='25%' height='1em' />
+              //              <rect transform='translate(20, 530)' width='25%' height='1em' />
+              // </svg>
+              // </section>
+              // `)
+              skeletonSection.innerHTML = `
               <section>
               <svg width='100%' height='90vh' class="skeletor">
                            <rect width='100%' height='30vh' />
@@ -52,7 +65,7 @@ import { loader } from "./modules/loading.js"
                            <rect transform='translate(20, 530)' width='25%' height='1em' />
               </svg>
               </section>
-              `)
+              `
 
               getMyData(video, getURL, loadingElement)
            
